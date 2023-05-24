c4214e53b26c1239d031744f18ebbc3a29e458ac
getssh <gtamrat33@gmail.com>
2023-05-25 01:58:19 +0300

    Add student class

---
+++ student.rb
@@ -0,0 +1,14 @@
+require_relateive 'person'
+
+class Student < Person
+  attr_accessor :classroom
+
+  def initialize(classroom)
+    super()
+    @classroom = classroom
+  end
+
+  def play_hooky()
+    "¯\(ツ)/¯"
+  end
+end
