a8a94853cb882dbf46f44101feb3e0ec95390aaf
getssh <gtamrat33@gmail.com>
2023-05-25 02:01:45 +0300

    Fix typo and add values to super class

--- student.rb
+++ student.rb
@@ -1,14 +1,14 @@
-require_relateive 'person'
+require_relative 'person'
 
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom)
-    super()
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
-  def play_hooky()
+  def play_hooky
     "¯\(ツ)/¯"
   end
 end
