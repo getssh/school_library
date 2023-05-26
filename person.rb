e4755d165ed03ecc9825325da9468fd1e5a7c407
getssh <gtamrat33@gmail.com>
2023-05-26 22:01:50 +0300

    Update person to inhereit from Nameable and method correct_name

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative 'nameable.rb'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -12,6 +14,9 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
 
   private
 
