af0475c907d9fd00a495aa0f7dc7cb1c39f5e14a
getssh <gtamrat33@gmail.com>
2023-05-28 03:34:29 +0300

    Add app class and run method

--- app.rb
+++ app.rb
@@ -3,3 +3,43 @@ require_relative 'student'
 require_relative 'teacher'
 require_relative 'rental'
 
+class App
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
+  def run
+    puts 'Welcome to School Library App!'
+    loop do
+      puts 'Please choose an option by entering a number:'
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals for a given person ID'
+      puts '7 - Exit'
+
+      option = gets.chomp.to_i
+      case option
+      when 1
+        list_all_books
+      when 2
+        list_all_people
+      when 3
+        create_person
+      when 4
+        create_book
+      when 5
+        create_rental
+      when 6
+        list_rentals_for_person
+      when 7
+        exit_app
+      else
+        puts 'Invalid option. Please choose a valid option.'
+      end
+    end
+  end
