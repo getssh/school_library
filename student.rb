ae26d92e50be623959870b509d84fa9126ffe066
getssh <gtamrat33@gmail.com>
2023-05-28 03:49:48 +0300

    Update classroom in constructior

--- student.rb
+++ student.rb
@@ -3,9 +3,9 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    assign_classroom(classroom)
+    @classroom = nil
   end
 
   def assign_classroom(classroom)
